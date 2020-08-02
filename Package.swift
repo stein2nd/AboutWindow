80f6e2bf2f346bea7964c72bb5091fc05796fb21
stein2nd <stein2nd@gmail.com>
2020-08-02 20:58:24 +0900

    Changed ".package(url:branch:)" to ".package(url:.branch(:))".

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package: Package = Package.init(
 	dependencies: [
 		// Dependencies declare other packages that this package depends on.
 		.package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.1"),
-		.package(url: "https://github.com/stein2nd/SwiftTryCatch.git", branch: "master"),
-		//.package(url: "https://github.com/stein2nd/TransparentScroller.git", branch: "master"),
+		.package(url: "https://github.com/stein2nd/SwiftTryCatch.git", .branch("master")),
+		.package(url: "https://github.com/stein2nd/TransparentScroller.git", .branch("master")),
 	],
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
-		.target(name: "AboutWindow", dependencies: []),
+		.target(name: "AboutWindow", dependencies: [ "XCGLogger", "SwiftTryCatch", "TransparentScroller" ]),
 		.testTarget(name: "AboutWindowTests", dependencies: [ "AboutWindow" ]),
 	]
 )
